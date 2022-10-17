61b7a1d6ba41b8743e249bc9bb930a3e977dbc68
Jon Massey <jon.massey@thedatalab.org>
2022-10-17 12:02:06 +0100

    fix unzip arg order

--- unzip.sh
+++ unzip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ls zipfiles/*.zip|awk -F'.zip' '{printf "unzip "$0" -d ";sub(/zipfiles/,"dmd",$1);print $1 " -f"}'|sh
+ls zipfiles/*.zip|awk -F'.zip' '{printf "unzip -f "$0" -d ";sub(/zipfiles/,"dmd",$1);print $1}'|sh
