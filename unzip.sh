91194a63ce24fcdaa9065da4fb2bf01ea8e45ae5
Jon Massey <jon.massey@thedatalab.org>
2023-01-12 11:50:36 +0000

    overwrite not freshen

--- unzip.sh
+++ unzip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ls zipfiles/*.zip|awk -F'.zip' '{printf "unzip -f "$0" -d ";sub(/zipfiles/,"dmd",$1);print $1}'|sh
+ls zipfiles/*.zip|awk -F'.zip' '{printf "unzip -o "$0" -d ";sub(/zipfiles/,"dmd",$1);print $1}'|sh
